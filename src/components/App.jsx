6b8acc77e2fd37e9b6880f3e4cc83932196edc52
ben <isieldor@gmail.com>
2018-10-31 15:23:58 -0500

    finally reached a place where development can continue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import ListItem from "./ListItem.jsx";
+let testJson = require("./test.json");
 
 const parkingStatusUrl =
   "https://us-central1-utdapi-217616.cloudfunctions.net/function-1";
 
-let myRequest = new Request("./test.json");
 fetch("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json")
   .then(function(response) {
     return response.json();
@@ -12,24 +12,13 @@ fetch("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json")
   .then(function(myJson) {
     console.log(JSON.stringify(myJson));
   });
-
-/*let statusJSon = fetch("./test.json");
-for (let i of statusJSon) {
-  i.json().then(res => console.log(res));
-}*/
+console.log(testJson);
 
 class App extends Component {
   render(props) {
     return (
       <div>
         <h1>Prop: {this.props.name}</h1>
-        {(function() {
-          let rows = [];
-          for (let i = 0; i < 4; i++) {
-            rows.push(<ListItem text={i} key={i} />);
-          }
-          return rows;
-        })()}
       </div>
     );
   }
